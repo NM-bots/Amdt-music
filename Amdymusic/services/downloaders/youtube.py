f99821c1a8f65d0523f68fcb17b25deb938c56d7
Nadun malaka <89643714+NM-bots@users.noreply.github.com>
2021-09-09 11:03:47 -0700

    Update youtube.py

--- Amdymusic/services/downloaders/youtube.py
+++ Amdymusic/services/downloaders/youtube.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# Modified by Sadew Jayasekara
+# Modified by Nadun malaka
 
 from os import path
 
