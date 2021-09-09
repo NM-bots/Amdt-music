ab6377b9d5ececa7623f7ac86ea0ba5117788d5e
Nadun malaka <89643714+NM-bots@users.noreply.github.com>
2021-09-09 11:08:38 -0700

    Update __main__.py

--- Amdymusic/__main__.py
+++ Amdymusic/__main__.py
@@ -1,4 +1,4 @@
-# StreamMusic (Telegram bot project)
+# Amdymusic (Telegram bot project)
 # Copyright (C) 2021  Nadun malaka & Rojserbest
 
 # This program is free software: you can redistribute it and/or modify
