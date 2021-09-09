035bf0cf3b35e026a7152144981671b7e3775919
Nadun malaka <89643714+NM-bots@users.noreply.github.com>
2021-09-09 11:06:04 -0700

    Update config.py

--- Amdymusic/config.py
+++ Amdymusic/config.py
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# Modified by Sadew
+# Modified by Nadun malaka
 
 import os
 from os import getenv
