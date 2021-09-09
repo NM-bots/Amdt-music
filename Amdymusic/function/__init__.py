fa612ec679d6d729dcb3e58add905702981e46fb
Nadun malaka <89643714+NM-bots@users.noreply.github.com>
2021-09-09 10:38:26 -0700

    Update __init__.py

--- Amdymusic/function/__init__.py
+++ Amdymusic/function/__init__.py
@@ -1,5 +1,5 @@
-from StreamMusic.function.admins import admins
-from StreamMusic.function.admins import get
-from StreamMusic.function.admins import set
+from Amdymusic.function.admins import admins
+from Amdymusic.function.admins import get
+from Amdymusic.function.admins import set
 
 __all__ = ["set", "get", "admins"]
